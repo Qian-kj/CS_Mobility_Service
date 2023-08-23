04f1ba8401e8df0e15fa43eda90b12e114bb25b1
Qian-KJ <kejiang.qian@hotmail.com>
2023-08-23 15:25:10 +0100

    model & pylogit

--- scripts/mode_choice_nhts.py
+++ scripts/mode_choice_nhts.py
@@ -13,6 +13,7 @@ import json
 from collections import OrderedDict
 import copy
 import re
+import pylogit as pl
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
